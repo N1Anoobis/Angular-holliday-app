8f21217d0f85d612b6166d53751b49b0a0dc6e9c
bond bond <mrbond84@hotmail.com>
2021-12-24 06:46:45 -0800

    Add ngrx app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';   
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatInputModule} from '@angular/material/input';
+import { StoreModule } from '@ngrx/store';
+import { initialState, reducers, effects } from './app.state';
+import { RotService } from './rot.service';
+import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
+import { BananaComponent } from './banana/banana.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports:      [ 
+    BrowserModule, 
+    CommonModule,
+    FormsModule,
+    // RotService, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    StoreModule.forRoot(reducers, {initialState}),
+    EffectsModule.forRoot(effects),
   ],
-  imports: [
-    BrowserModule
+  declarations: [ 
+    AppComponent, 
+    BananaComponent 
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ 
+    AppComponent 
+  ]
 })
 export class AppModule { }
